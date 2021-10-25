538aab0dd22ddca6c1a25551342703970675d3d5
AmulyaVishnbhotla <64000166+AmulyaVishnbhotla@users.noreply.github.com>
2021-10-25 22:51:26 +0530

    Done with Finding Region

--- hw6_social.py
+++ hw6_social.py
@@ -102,7 +102,9 @@ Parameters: dataframe ; str
 Returns: str
 '''
 def getRegionFromState(stateDf, state):
-    return
+    row = stateDf.loc[stateDf['state'] == state,'region']
+    return (row.values[0])
+
 
 
 '''
@@ -293,7 +295,7 @@ if __name__ == "__main__":
     # test.week1Tests()
     # print("\n" + "#"*15 + " WEEK 1 OUTPUT " + "#" * 15 + "\n")
     # test.runWeek1()
-    test.testFindHashtags()
+    test.testGetRegionFromState()
     ## Uncomment these for Week 2 ##
     """print("\n" + "#"*15 + " WEEK 2 TESTS " +  "#" * 16 + "\n")
     test.week2Tests()
