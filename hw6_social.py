a07a07d933ad523f89634dd786db59a6eb84e99c
AmulyaVishnbhotla <64000166+AmulyaVishnbhotla@users.noreply.github.com>
2021-10-27 21:22:37 +0530

    Graph State Count Dictionaries

--- hw6_social.py
+++ hw6_social.py
@@ -306,7 +306,21 @@ Returns: None
 '''
 def graphStateCounts(stateCounts, title):
     import matplotlib.pyplot as plt
+    xlst = [i for i in stateCounts]
+    w = 0.8
+    ylst = [stateCounts[i] for i in stateCounts]
+    for index in range(len(ylst)):
+        plt.bar(xlst[index], ylst[index], width=w)
+    plt.xticks(ticks=list(range(len(xlst))), labels=xlst, rotation="vertical")
+
+    plt.show()
+
     return
+# df = makeDataFrame("data/politicaldata.csv")
+# stateDf = makeDataFrame("data/statemappings.csv")
+# addColumns(df, stateDf)
+# addSentimentColumn(df)
+# print(graphStateCounts(getDataCountByState(df, "", ""),"Sample"))
 
 
 '''
@@ -401,14 +415,14 @@ if __name__ == "__main__":
     # print("\n" + "#"*15 + " WEEK 1 OUTPUT " + "#" * 15 + "\n")
     # test.runWeek1()
     ## Uncomment these for Week 2 ##
-    print("\n" + "#"*15 + " WEEK 2 TESTS " +  "#" * 16 + "\n")
+    '''print("\n" + "#"*15 + " WEEK 2 TESTS " +  "#" * 16 + "\n")
     test.week2Tests()
     print("\n" + "#"*15 + " WEEK 2 OUTPUT " + "#" * 15 + "\n")
-    test.runWeek2()
+    test.runWeek2()'''
 
     ## Uncomment these for Week 3 ##
-    """print("\n" + "#"*15 + " WEEK 3 OUTPUT " + "#" * 15 + "\n")
-    test.runWeek3()"""
+    print("\n" + "#"*15 + " WEEK 3 OUTPUT " + "#" * 15 + "\n")
+    test.runWeek3()
     # df = makeDataFrame("data/politicaldata.csv")
     # stateDf = makeDataFrame("data/statemappings.csv")
     # addColumns(df, stateDf)
