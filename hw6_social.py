4df7d852fec3410f02d1260c8911691883836ef0
AmulyaVishnbhotla <64000166+AmulyaVishnbhotla@users.noreply.github.com>
2021-10-28 15:40:42 +0530

    made a change

--- hw6_social.py
+++ hw6_social.py
@@ -350,31 +350,19 @@ Returns: None
 '''
 from itertools import chain
 def graphRegionComparison(regionDicts, title):
-    # featurelst = []
-    # regionlst = []
-    # regionvalue = []
-    # #templist = []
-    # for i in regionDicts:
-    #     for j in regionDicts[i]:
-    #         if j not in featurelst:
-    #             featurelst.append(j)
-    #     regionlst.append(i)
-    # for i in regionDicts.keys():
-    #     templist = []
-    #     for k in featurelst:
-    #         for x in regionDicts[i].keys():
-    #             if k in x:
-    #                 templist.append(regionDicts[i][k])
-    #             else:
-    #                 templist.append(0)
-                
-        
-    # # regionvalue.append(templist)
-    # # print(regionvalue)
-    # # regionfeature = list(chain.from_iterable(regionvalue))
-    # # sideBySideBarPlots(featurelst, regionlst, regionfeature, "Region Comparison")
-    # # print("region feature", len(regionfeature))
-    # # print("region value", len(regionlst))
+    featurelst = []
+    regionlst = []
+    regionvalue = []
+    for i in regionDicts:
+        templist = []
+        x = regionDicts[i] #feature
+        for j in x:
+            if j not in featurelst:
+                featurelst.append(j)
+            templist.append(x[j])
+        regionvalue.append(templist)  
+        regionlst.append(i)
+    sideBySideBarPlots(featurelst, regionlst, regionvalue, title)
     return
 
 
